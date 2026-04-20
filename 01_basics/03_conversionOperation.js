4cac2adc5f911a4a03fe26642ecf6820705a7a9e
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-04-20 18:26:56 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "12abc";
+let scores = "12";
+let scoress = 12;
+
+
+
+
+console.log(typeof score);
+console.log(typeof scores);
+console.log(typeof (scoress));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);   // Nan   it is special type
+
+//"12"=>33
+//"12abc"=> Not an Number
+//true => 1; false =>0
+
+let isLoggedIn = 0
+let booleanISLoggedIn = Boolean (isLoggedIn)
+console.log(booleanISLoggedIn);
