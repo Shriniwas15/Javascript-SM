3f22eae41698c14b0c8f215892183522050681d9
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-04-25 18:23:30 +0000

    container config

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -3,5 +3,8 @@ console.log(2>1);
 console.log(2>"1")
 console.log(2>"4")
 
+console.log(undefined=0);  //undefined not allowed 
+console.log(undefined>0);
+
 // ===   strict check
 console.log("2"===2);
