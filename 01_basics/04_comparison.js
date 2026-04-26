f607e9e74a6d3969c1dd2a708b5c7090a18150b1
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-04-26 18:14:46 +0000

    config done

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -3,8 +3,25 @@ console.log(2>1);
 console.log(2>"1")
 console.log(2>"4")
 
-console.log(undefined=0);  //undefined not allowed 
-console.log(undefined>0);
+// console.log(undefined=0);  //undefined not allowed 
+// console.log(undefined>0);
 
 // ===   strict check
 console.log("2"===2);
+
+
+//--reverse string
+function reverse(str){
+    return str.split('').reverse().join('');
+    }
+    console.log(reverse("hello"); // olleh
+     
+// }
+
+// // ======================
+// // 2. Palindrome
+// // ======================
+// function isPalindrome(str){
+//   return str === str.split('').reverse().join('');
+// }
+// console.log("2 Palindrome:", isPalindrome("madam")); // true
