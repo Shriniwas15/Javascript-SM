3e17a330901de3ab0053c7e0739d174fd75f3a47
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-05-29 18:31:50 +0000

    config done

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,18 @@
+//Primitive
+
+//7 types  : String ,Number,null,undefined,boolean,symbol,bigint
+
+//Reference types (Non-Primitive) : Object,Array,Function
+
+// console.log(typeof "hello"); // string
+// console.log(typeof 123); // number
+// console.log(typeof true); // boolean
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object (this is a quirk in JavaScript)
+// console.log(typeof Symbol("id")); // symbol
+// console.log(typeof 123n); // bigint
+
+// console.log(typeof {name:"Alice"}); // object
+// console.log(typeof [1,2,3]); // object (arrays are objects in JavaScript)
+// console.log(typeof function(){}); // function
+
