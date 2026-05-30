a96fb8c4ad4a29131834ced23c18c028245846f6
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-05-30 02:36:27 +0000

    datatypes summery

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -16,3 +16,41 @@
 // console.log(typeof [1,2,3]); // object (arrays are objects in JavaScript)
 // console.log(typeof function(){}); // function
 
+
+
+//-------------------------Memory--------------------------------------------------------
+
+//Stack -primitive , Heap - non-primitive (reference types)
+
+//Primitive types are stored directly in the variable,
+//  while reference types store a reference to the location in memory where the data is stored.
+//  When you assign a primitive value to a variable, it creates a copy of that value.
+//  When you assign a reference type to a variable, it creates a reference to the same object in memory.
+//  This means that changes to the object through one variable will affect all variables that reference that object.
+
+//////////Example of Primitive type
+let myYoutubename = "shridotcom";
+let anotherName = myYoutubename; // anotherName is a copy of the value in myYoutubename
+console.log(myYoutubename);// Output: shridotcom
+
+myYoutubename = "shridotcom2"; // Changing myYoutubename does not affect anotherName
+console.log(myYoutubename); // Output: shridotcom2
+
+
+let myYoutubename1 = "shridotcom";
+let anotherName1 = myYoutubename;
+anotherName1 = "shridotcom3"; 
+console.log(myYoutubename1); // Output: shridotcom
+
+
+
+// --------------------------Example of Reference type--------------------------------------------------------
+let userOne={
+    email:"abc@example.com",
+    upi:"abc@upi"
+}
+let userTwo = userOne; // userTwo is a reference to the same object in memory as userOne
+
+userTwo.email = "xyz@example.com";
+console.log(userOne.email); // Output: xyz@example.com
+console.log(userTwo.email); // Output: xyz@example.com
