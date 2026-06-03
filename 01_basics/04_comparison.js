4e507e5d046b75ecf1b396e91574fe5a993f2bfc
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-06-03 18:38:37 +0000

    update

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -566,3 +566,52 @@ pubsub.subscribe("news", data=>{
 });
 
 pubsub.publish("news","Breaking News!");
+
+// 37   
+// Implement a Simple LRU Cache
+// LRU (Least Recently Used) Cache evicts the least recently used item when capacity is exceeded.
+
+
+class LRUCache{
+
+  constructor(capacity){
+    this.capacity = capacity;
+    this.cache = new Map();
+  }
+
+  get(key){
+    if(!this.cache.has(key)){
+      return -1;
+    }
+
+    const value = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
+
+  put(key, value){
+    if(this.cache.has(key)){
+      this.cache.delete(key);
+    }
+    else if(this.cache.size === this.capacity){
+      const firstKey = this.cache.keys().next().value;
+      this.cache.delete(firstKey);
+    }
+    this.cache.set(key, value);
+  }
+
+}
+
+const lru = new LRUCache(2);
+
+lru.put(1,1);
+lru.put(2,2);
+console.log(("37   ")+lru.get(1));
+lru.put(3,3);
+console.log(("37   ")+lru.get(2));
+lru.put(4,4);
+console.log(("37   ")+lru.get(3));
+console.log(("37   ")+lru.get(4));
+
+//38. Implement a Simple Event Loop
