5002a79b25c43e97511c4945e17e5ee8c44e8445
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-07-05 19:03:56 +0000

    update code

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -2010,6 +2010,35 @@ console.log(("67   ")+ JSON.stringify(graphListWeighted.getNeighbors("A"))); // 
 console.log(("67   ")+ JSON.stringify(graphListWeighted.getNeighbors("B"))); // [{"node":"A","weight":5},{"node":"D","weight":2}]
 console.log(("67   ")+ JSON.stringify(graphListWeighted.getNeighbors("E"))); // [] (no neighbors)
 
+//68 Implement a Simple Graph with Adjacency Matrix and Weights
+
+class GraphMatrixWeighted{
+
+  constructor(size){
+    this.size = size;
+    this.matrix = new Array(size).fill().map(() => new Array(size).fill(0));
+  }
+
+  addEdge(v1, v2, weight){
+    this.matrix[v1][v2] = weight;
+    this.matrix[v2][v1] = weight; // For undirected graph
+  }
+
+  getWeight(v1, v2){
+    return this.matrix[v1][v2];
+  }
+
+}
+
+const graphMatrixWeighted = new GraphMatrixWeighted(5);
+
+graphMatrixWeighted.addEdge(0, 1, 5);
+graphMatrixWeighted.addEdge(0, 2, 3);
+graphMatrixWeighted.addEdge(1, 3, 2);
+
+console.log(("68   ")+ graphMatrixWeighted.getWeight(0, 1)); // 5
+console.log(("68   ")+ graphMatrixWeighted.getWeight(0, 3)); //
+
 
 
   
