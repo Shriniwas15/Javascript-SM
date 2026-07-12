79f65c8e4ed384bc3a6eefbd3b2355de1010817a
Shriniwas  Mankeshwar <132052105+Shriniwas15@users.noreply.github.com>
2026-07-12 19:05:35 +0000

    update code

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -2242,6 +2242,45 @@ graphSetWeighted2.addEdge("B", "D", 2);
 console.log(("74   ")+ graphSetWeighted2.getWeight("A", "B")); // 5
 console.log(("74   ")+ graphSetWeighted2.getWeight("A", "D")); // 0 (no edge)
 
+//75 Implement a Simple Graph with Adjacency Map and Weights
+
+class GraphMapWeighted2{
+
+  constructor(){
+    this.adjacencyMap = new Map();
+  }
+
+  addVertex(vertex){
+    if(!this.adjacencyMap.has(vertex)){
+      this.adjacencyMap.set(vertex, new Map());
+    }
+  }
+
+  addEdge(v1, v2, weight){
+    this.addVertex(v1);
+    this.addVertex(v2);
+    this.adjacencyMap.get(v1).set(v2, weight);
+    this.adjacencyMap.get(v2).set(v1, weight); // For undirected graph
+  }
+
+  getWeight(v1, v2){
+    const neighbors = this.adjacencyMap.get(v1);
+    return neighbors ? neighbors.get(v2) : 0;
+  }
+
+}
+
+const graphMapWeighted2 = new GraphMapWeighted2();
+
+graphMapWeighted2.addEdge("A", "B", 5);
+graphMapWeighted2.addEdge("A", "C", 3);
+graphMapWeighted2.addEdge("B", "D", 2);
+
+console.log(("75   ")+ graphMapWeighted2.getWeight("A", "B")); // 5
+console.log(("75   ")+ graphMapWeighted2.getWeight("A", "D")); // 0 (no edge)   
+
+
+
 
   
   
